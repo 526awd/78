284efaeecf2c6f2ffcd6e23e360510b5133fe14c
526awd <13371088405@163.com>
2025-08-09 21:47:20 +0800

    y

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import os
-inp="1.mp4"
+inp="studio_video_1754743955890.mp4"
 to="zundamon.mp4"
 si=99
 os.system("cp "+inp+" "+to)
 for i in range(si):
-    os.system("ffmpeg -f concat -i 1.mp4 -i "+to+"-c copy "+to)
+    os.system("ffmpeg -f concat -i -safe 0 "+inp+" -i "+to+"-c copy "+to)
