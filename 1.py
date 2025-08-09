f366722d16355ff364c524019e28814488b99039
526awd <13371088405@163.com>
2025-08-10 05:04:08 +0800

    dv

--- 1.py
+++ 1.py
@@ -5,6 +5,6 @@ si=99
 os.system("cp "+inp+" "+to)
 for i in range(si):
     os.system("ffmpeg -f concat -safe 0 -i input.txt -c copy "+to)
-    os.system("rm zundamon.mp4"+)
+    os.system("rm zundamon.mp4")
     os.system("cp zundamon1.mp4 zundamon.mp4")
     os.system("rm zundamon1.mp4")
