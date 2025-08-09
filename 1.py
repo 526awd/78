283698a5268983ca8e4fc432d1eaaf89117ec16c
526awd <13371088405@163.com>
2025-08-10 05:01:37 +0800

    dv

--- 1.py
+++ 1.py
@@ -1,7 +1,10 @@
 import os
 inp="1.mp4"
-to="zundamon.mp4"
+to="zundamon1.mp4"
 si=99
 os.system("cp "+inp+" "+to)
 for i in range(si):
     os.system("ffmpeg -f concat -safe 0 -i input.txt -c copy "+to)
+    os.system("rm zundamon.mp4"+)
+    os.system("cp zundamon1.mp4 zundamon.mp4")
+    os.system("rm zundamon1.mp4")
