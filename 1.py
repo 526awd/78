3dc89de745c2724af4be5578ec2a04ab42a23aa7
526awd <13371088405@163.com>
2025-08-10 04:50:17 +0800

    dv

--- 1.py
+++ 1.py
@@ -4,4 +4,4 @@ to="zundamon.mp4"
 si=99
 os.system("cp "+inp+" "+to)
 for i in range(si):
-    os.system("ffmpeg -f concat -safe 0 -i  "+inp+" -i "+to+"-c copy "+to)
+    os.system("ffmpeg -f concat -safe 0 -i input.txt -c copy "+to)
